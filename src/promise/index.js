7bb321d2ace70d2adb02c3ecb28b9b67272db160
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-24 12:02:17 -0700

    finished excercise using then, catch, finally

--- src/promise/index.js
+++ src/promise/index.js
@@ -2,7 +2,7 @@ const promise = new Promise(function(resolve, reject){
     resolve('Hey!')
 });
 
-const cows = 9;
+const cows = 15;
 
 const countCows = new Promise(function(resolve, reject) {
     if (cows > 10) {
@@ -14,4 +14,6 @@ const countCows = new Promise(function(resolve, reject) {
 
 countCows.then((result) => {
     console.log(result);
-}).catch((error) => {console.log(error);})
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log('Finally!'));
