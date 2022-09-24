dfa7ef72cf5413411659cda267c79ca5c8a73554
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-24 11:21:40 -0700

    add fetch data function

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,2 +1,20 @@
-const XMLHttprequest = require('xmlhttprequest');
+const XMLHttprequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1'
+
+function fetchData(urlApi, callback) {
+    let xhttp = new XMLHttprequest();
+
+    xhttp.open('GET', urlApi, true)
+
+    xhttp.onreadystatechange = function (event) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText));
+            }
+        } else {
+            const error = new Error('Error ' + urlApi);
+            return callback(error, null);
+        }
+    }
+    xhttp.send();
+}
