09fc380e325a75fb99c016ae6fd060c2f4207275
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-24 12:00:16 -0700

    fix typo

--- src/promise/index.js
+++ src/promise/index.js
@@ -2,16 +2,16 @@ const promise = new Promise(function(resolve, reject){
     resolve('Hey!')
 });
 
-const cows = 15;
+const cows = 9;
 
 const countCows = new Promise(function(resolve, reject) {
     if (cows > 10) {
         resolve(`We have ${cows} cows on the farm.`);
     } else {
-        reject("There is not enough cows on the farm");
+        reject("There is not enought cows on the farm");
     }
 });
 
 countCows.then((result) => {
     console.log(result);
-})
+}).catch((error) => {console.log(error);})
