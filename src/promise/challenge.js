0910e9ffbbd0fd44060cfd400b04f5b7b304a67b
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-25 13:35:17 -0700

    finished fetch challenge

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -5,12 +5,30 @@ function fetchData(urlApi) {
     return fetch(urlApi);
 };
 
+// fetchData(`${API}/products`)
+//     .then(response => response.json())
+//     .then(products => {
+//         console.log(products);
+//     })
+//     .then(() => {
+//         console.log("Hello");
+//     })
+//     .catch(error => console.log(error));
+
 fetchData(`${API}/products`)
     .then(response => response.json())
     .then(products => {
-        console.log(products);
+        console.log(products)
+        return fetchData(`${API}/products/${products[0].id}`)
     })
-    .then(() => {
-        console.log("Hello");
+    .then(response => response.json())
+    .then(product => {
+        console.log(product.title)
+        return fetchData(`${API}/categories/${product.category.id}`)
     })
-    .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(category => {
+            console.log(category.name)
+    })
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finally'))
