9eede8222fa03f704c0e26709b7c243b9c224e97
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-26 19:15:38 -0700

    finish promise challenge

--- src/promise/challenge-post.js
+++ src/promise/challenge-post.js
@@ -2,5 +2,26 @@ import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
 
 function postData(urlApi, data) {
-    const response = fetch
+    const response = fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    return response;
 }
+
+const dataExample = {
+    "title": "Pikaplush",
+    "price": 25,
+    "description": "A confy plush non-related to the big 'Mon company",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+};
+
+postData(`${API}/products`, dataExample)
+    .then(response => response.json())
+    .then(data => console.log(data))
