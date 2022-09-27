8286e93b08861b833e64b00833ebfeb3eafc9c25
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-26 20:12:28 -0700

    add fetchData async Fn

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -1,3 +1,8 @@
 import fetch from "node-fetch";
 const API = 'https://api.escuelajs.co/api/v1';
 
+async function fetchData(urlAPI) {
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+    return data;
+}
