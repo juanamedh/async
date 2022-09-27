c5c0744773e5afe0b92cf8bfa557be9648cc3084
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-09-26 20:24:13 -0700

    finish challenge

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -6,3 +6,22 @@ async function fetchData(urlAPI) {
     const data = await response.json();
     return data;
 }
+
+const anotherFn = async () => {
+    try {
+        const products = await fetchData(`${API}/products`);
+        const product = await fetchData(`${API}/products/${products[0].id}`);
+        const category = await fetchData(`${API}/categories/${product.category.id}`);
+        
+
+        console.log(products);
+        console.log(product.title);
+        console.log(category.name);
+
+    }catch(error) {
+        console.error(error);
+
+    }
+}
+
+anotherFn(API)
